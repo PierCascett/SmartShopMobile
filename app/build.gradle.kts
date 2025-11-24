fd9ed542f66637b9533b48f5fe69c4cdd182e41c
Pier2690 <156895742+Pier2690@users.noreply.github.com>
2025-11-24 19:31:00 +0100

    Aggiornate le versioni minSdk e Java/Kotlin
    Per evitare errori di versioni, anche se non generavano alcun tipo di problema in particolare

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,3 @@
-
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -12,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "it.unito.smartshopmobile"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -31,11 +30,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
